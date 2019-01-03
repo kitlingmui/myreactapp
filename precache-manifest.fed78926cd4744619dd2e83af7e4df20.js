e9eb0adac58789310e47b87b9560957dd2bfba06
kitlingmui <kitlingmui@gmail.com>
2019-01-03 01:44:56 -0800

    Updates

---
+++ precache-manifest.fed78926cd4744619dd2e83af7e4df20.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myreactapp/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "0ce04c2388bba3e2f0cf",
+    "url": "/myreactapp/static/js/runtime~main.0ce04c23.js"
+  },
+  {
+    "revision": "f47c0e2fbd33623703a7",
+    "url": "/myreactapp/static/js/main.f47c0e2f.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/myreactapp/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "f47c0e2fbd33623703a7",
+    "url": "/myreactapp/static/css/main.cb613a0a.chunk.css"
+  },
+  {
+    "revision": "01a5373bd4b139ba419c072fcb12296f",
+    "url": "/myreactapp/index.html"
+  }
+];
